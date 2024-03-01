b0e31b85b434b3c8ad6a68052824d9e163f39362
Vicknesh Suppramaniam <vicknesh.suppramaniam@gmail.com>
2024-03-01 15:47:28 +0800

    new start instructions to set the correct PUID and PGID for the data files

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
+if [ -z ${PUID} ]
+then
+    PUID=1000
+fi
+
+if [ -z ${PGID} ]
+then
+    PGID=1000
+fi
+
+usermod -u ${PUID} lhs >/dev/null
+groupmod -g ${PGID} lhs >/dev/null
+chown -R lhs:lhs /home/lhs
+
 export PATH=$PATH:$HOME/hsj/bin
 
 if [ ! -f /home/lhs/data/config.dct ]
 then
-    /home/lhs/hsj/bin/hdl-setup-server /home/lhs/data
+    gosu ${PUID}:${PGID} /home/lhs/hsj/bin/hdl-setup-server /home/lhs/data
     sleep 3
 fi
 
-/home/lhs/hsj/bin/hdl-server /home/lhs/data
+rm -f /home/lhs/data/txns/lock
+gosu ${PUID}:${PGID} /home/lhs/hsj/bin/hdl-server /home/lhs/data
